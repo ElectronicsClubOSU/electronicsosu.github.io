f3311bf067a3f7ab83e75550c1c1e22048dd6bd0
Chris Murray <cdmurray08@gmail.com>
2019-09-06 19:44:52 -0400

    Update format-google-calendar.js

--- assets/js/format-google-calendar.js
+++ assets/js/format-google-calendar.js
@@ -324,9 +324,8 @@ var formatGoogleCalendar = (function() {
         pastTopN: -1,
         upcomingTopN: -1,
         itemsTagName: 'li',
-        // Set date range to choose events from
-        timeMin: '2019-09-06T18:30:00 -05:00',
-        timeMax: '2020-09-06T22:00:00 -05:00'
+        timeMin: '2019-09-06T18:38:10 -05:00',
+        timeMax: '2020-09-06T20:38:10 -05:00'
         upcomingSelector: '#events-upcoming',
         pastSelector: '#events-past',
         upcomingHeading: '<h2>Upcoming events</h2>',
